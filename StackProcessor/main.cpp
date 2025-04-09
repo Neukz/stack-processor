eea7cd4571eca6ecf32f0667f1fe65eff6f279a2
Neukz <kacperneumann1@gmail.com>
2025-04-09 20:39:42 +0200

    Handle instruction execution with a loop instead of recursion

--- StackProcessor/main.cpp
+++ StackProcessor/main.cpp
@@ -423,190 +423,187 @@ public:
     }
 
     void execute() {
-        if (instructionPointer >= programLength) {
-            return;
-        }
+        while (instructionPointer < programLength) {
+            char instruction = program[instructionPointer];
+            switch (instruction) {
+                case '\'':
+                {
+                    stack->push(new List());
+                    break;
+                }
+                case ',':
+                {
+                    stack->pop();
+                    break;
+                }
+                case ':':
+                {
+                    List* topList = stack->pop()->list;
+                    List* copiedList = topList->copy();
+                    stack->push(topList);
+                    stack->push(copiedList);
+                    break;
+                }
+                case ';':
+                {
+                    List* upperList = stack->pop()->list;
+                    List* lowerList = stack->pop()->list;
+                    stack->push(upperList);
+                    stack->push(lowerList);
+                    break;
+                }
+                case '@':
+                {
+                    int targetListIndex = stack->pop()->list->toInt();
+                    List* targetList = copyListAt(targetListIndex);
+                    stack->push(targetList);
+                    break;
+                }
+                case '.':
+                {
+                    char input;
+                    std::cin >> input;
+                    List* topList = stack->pop()->list;
+                    topList->addFirst(new Node{ input });
+                    stack->push(topList);
+                    break;
+                }
+                case '>':
+                {
+                    StackElement* topElement = stack->pop();
+                    if (topElement != nullptr) {
+                        std::cout << topElement->list->head->data;
+                    }
+                    break;
+                }
+                case '!':
+                {
+                    List* topList = stack->pop()->list;
+                    List* newTopList = new List();
+                    if (topList->head == nullptr || (topList->head->data == '0' && topList->head->next == nullptr)) {
+                        newTopList->addFirst(new Node{ '1' });
+                    } else {
+                        newTopList->addFirst(new Node{ '0' });
+                    }
+                    stack->push(newTopList);
+                    break;
+                }
+                case '<':
+                {
+                    List* listA = stack->pop()->list;
+                    List* listB = stack->pop()->list;
+                    List* newTopList = new List();
+                    newTopList->addFirst(new Node{ listB->lessThan(listA) ? '1' : '0' });
+                    stack->push(newTopList);
+                    break;
+                }
+                case '=':
+                {
+                    List* listA = stack->pop()->list;
+                    List* listB = stack->pop()->list;
+                    List* newTopList = new List();
+                    newTopList->addFirst(new Node{ listB->equals(listA) ? '1' : '0' });
+                    stack->push(newTopList);
+                    break;
+                }
+                case '~':
+                {
+                    List* newTopList = List::fromNumber(instructionPointer);
+                    stack->push(newTopList);
+                    break;
+                }
+                case '?':
+                {
+                    int t = stack->pop()->list->toInt();
+                    List* listW = stack->pop()->list;
+                    if (listW->head != nullptr && !(listW->head->data == '0' && listW->head->next == nullptr)) {
+                        instructionPointer = t;
+                        continue;
+                    }
+                    break;
+                }
+                case '-':
+                {
+                    List* topList = stack->pop()->list;
+                    Node* lastNode = topList->getLast();
+                    if (lastNode != nullptr && topList->getLast()->data == '-') {
+                        topList->removeLast();
+                    } else {
+                        topList->addLast(new Node{ '-' });
+                    }
+                    stack->push(topList);
+                    break;
+                }
+                case '^':
+                {
+                    List* topList = stack->pop()->list;
+                    Node* lastNode = topList->getLast();
+                    if (lastNode != nullptr && topList->getLast()->data == '-') {
+                        topList->removeLast();
+                    }
+                    stack->push(topList);
+                    break;
+                }
+                case '$':
+                {
+                    List* topList = stack->pop()->list;
+                    Node* firstNode = topList->head;
+                    topList->removeFirst();
+                    stack->push(topList);
+                    List* newTopList = new List();
+                    newTopList->addFirst(firstNode);
+                    stack->push(newTopList);
+                    break;
+                }
+                case '#':
+                {
+                    List* upperList = stack->pop()->list;
+                    List* lowerList = stack->pop()->list;
+                    Node* lowerListLastNode = lowerList->getLast();
+                    lowerListLastNode->next = upperList->head;
+                    upperList->head->prev = lowerListLastNode;
+                    stack->push(lowerList);
+                    break;
+                }
+                case '+':
+                {
+                    List* listA = stack->pop()->list;
+                    List* listB = stack->pop()->list;
+                    List* sumList = listA->addition(listB);
+                    stack->push(sumList);
+                    break;
+                }
+                case '&':
+                {
+                    stack->print();
+                    break;
+                }
+                case ']':
+                {
+                    int ascii = stack->pop()->list->toInt();
+                    List* newTopList = new List();
+                    newTopList->addFirst(new Node{ (char)ascii });
+                    stack->push(newTopList);
+                    break;
+                }
+                case '[':
+                {
+                    List* asciiList = List::fromNumber((int)stack->pop()->list->head->data);
+                    stack->push(asciiList);
+                    break;
+                }
+                default:
+                {
+                    StackElement* topElement = stack->pop();
+                    if (topElement != nullptr) {
+                        topElement->list->addFirst(new Node{ instruction });
+                        stack->push(topElement->list);
+                    }
+                }
+            }
 
-        char instruction = program[instructionPointer];
-        switch (instruction) {
-            case '\'':
-            {
-                stack->push(new List());
-                break;
-            }
-            case ',':
-            {
-                stack->pop();
-                break;
-            }
-            case ':':
-            {
-                List* topList = stack->pop()->list;
-                List* copiedList = topList->copy();
-                stack->push(topList);
-                stack->push(copiedList);
-                break;
-            }
-            case ';':
-            {
-                List* upperList = stack->pop()->list;
-                List* lowerList = stack->pop()->list;
-                stack->push(upperList);
-                stack->push(lowerList);
-                break;
-            }
-            case '@':
-            {
-                int targetListIndex = stack->pop()->list->toInt();
-                List* targetList = copyListAt(targetListIndex);
-                stack->push(targetList);
-                break;
-            }
-            case '.':
-            {
-                char input;
-                std::cin >> input;
-                List* topList = stack->pop()->list;
-                topList->addFirst(new Node{ input });
-                stack->push(topList);
-                break;
-            }
-            case '>':
-            {
-                StackElement* topElement = stack->pop();
-                if (topElement != nullptr) {
-                    std::cout << topElement->list->head->data;
-                }
-                break;
-            }
-            case '!':
-            {
-                List* topList = stack->pop()->list;
-                List* newTopList = new List();
-                if (topList->head == nullptr || (topList->head->data == '0' && topList->head->next == nullptr)) {
-                    newTopList->addFirst(new Node{ '1' });
-                } else {
-                    newTopList->addFirst(new Node{ '0' });
-                }
-                stack->push(newTopList);
-                break;
-            }
-            case '<':
-            {
-                List* listA = stack->pop()->list;
-                List* listB = stack->pop()->list;
-                List* newTopList = new List();
-                newTopList->addFirst(new Node{ listB->lessThan(listA) ? '1' : '0' });
-                stack->push(newTopList);
-                break;
-            }
-            case '=':
-            {
-                List* listA = stack->pop()->list;
-                List* listB = stack->pop()->list;
-                List* newTopList = new List();
-                newTopList->addFirst(new Node{ listB->equals(listA) ? '1' : '0' });
-                stack->push(newTopList);
-                break;
-            }
-            case '~':
-            {
-                List* newTopList = List::fromNumber(instructionPointer);
-                stack->push(newTopList);
-                break;
-            }
-            case '?':
-            {
-                int t = stack->pop()->list->toInt();
-                List* listW = stack->pop()->list;
-                if (listW->head != nullptr && !(listW->head->data == '0' && listW->head->next == nullptr)) {
-                    instructionPointer = t;
-                    return execute();
-                }
-                break;
-            }
-            case '-':
-            {
-                List* topList = stack->pop()->list;
-                Node* lastNode = topList->getLast();
-                if (lastNode != nullptr && topList->getLast()->data == '-') {
-                    topList->removeLast();
-                } else {
-                    topList->addLast(new Node{ '-' });
-                }
-                stack->push(topList);
-                break;
-            }
-            case '^':
-            {
-                List* topList = stack->pop()->list;
-                Node* lastNode = topList->getLast();
-                if (lastNode != nullptr && topList->getLast()->data == '-') {
-                    topList->removeLast();
-                }
-                stack->push(topList);
-                break;
-            }
-            case '$':
-            {
-                List* topList = stack->pop()->list;
-                Node* firstNode = topList->head;
-                topList->removeFirst();
-                stack->push(topList);
-                List* newTopList = new List();
-                newTopList->addFirst(firstNode);
-                stack->push(newTopList);
-                break;
-            }
-            case '#':
-            {
-                List* upperList = stack->pop()->list;
-                List* lowerList = stack->pop()->list;
-                Node* lowerListLastNode = lowerList->getLast();
-                lowerListLastNode->next = upperList->head;
-                upperList->head->prev = lowerListLastNode;
-                stack->push(lowerList);
-                break;
-            }
-            case '+':
-            {
-                List* listA = stack->pop()->list;
-                List* listB = stack->pop()->list;
-                List* sumList = listA->addition(listB);
-                stack->push(sumList);
-                break;
-            }
-            case '&':
-            {
-                stack->print();
-                break;
-            }
-            case ']':
-            {
-                int ascii = stack->pop()->list->toInt();
-                List* newTopList = new List();
-                newTopList->addFirst(new Node{ (char)ascii });
-                stack->push(newTopList);
-                break;
-            }
-            case '[':
-            {
-                List* asciiList = List::fromNumber((int)stack->pop()->list->head->data);
-                stack->push(asciiList);
-                break;
-            }
-            default:
-            {
-                StackElement* topElement = stack->pop();
-                if (topElement != nullptr) {
-                    topElement->list->addFirst(new Node{ instruction });
-                    stack->push(topElement->list);
-                }
-            }
+            instructionPointer++;
         }
-
-        instructionPointer++;
-        execute();
     }
 };
 
